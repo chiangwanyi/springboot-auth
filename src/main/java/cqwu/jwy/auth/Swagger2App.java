5045d43b068ecb405a0159cc123cc2e16efd25e4
chiangwanyi <chiangwanyi@163.com>
2020-06-08 11:53:17 +0800

    v0.0.1

--- src/main/java/cqwu/jwy/auth/Swagger2App.java
+++ src/main/java/cqwu/jwy/auth/Swagger2App.java
@@ -10,29 +10,29 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 public class Swagger2App {
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                //为当前包路径
-                .apis(RequestHandlerSelectors.basePackage("com.template.controller"))
-                .paths(PathSelectors.any())
-                .build();
-//        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
-    }
-
-    //构建 api文档的详细信息函数,注意这里的注解引用的是哪个
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                //页面标题
-                .title("Spring Boot 使用 Swagger2 构建RESTful API")
-                //创建人
-                .contact(new Contact("Bryan", "http://blog.bianxh.top/", ""))
-                //版本号
-                .version("1.0")
-                //描述
-                .description("API 描述")
-                .build();
-    }
+//    @Bean
+//    public Docket createRestApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .select()
+//                //为当前包路径
+//                .apis(RequestHandlerSelectors.basePackage("com.template.controller"))
+//                .paths(PathSelectors.any())
+//                .build();
+////        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
+//    }
+//
+//    //构建 api文档的详细信息函数,注意这里的注解引用的是哪个
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                //页面标题
+//                .title("Spring Boot 使用 Swagger2 构建RESTful API")
+//                //创建人
+//                .contact(new Contact("Bryan", "http://blog.bianxh.top/", ""))
+//                //版本号
+//                .version("1.0")
+//                //描述
+//                .description("API 描述")
+//                .build();
+//    }
 }
